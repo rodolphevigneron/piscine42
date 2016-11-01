80607fcb97b8d11e4c20e324e4d62c175be328a9
Guillaume LARIVIERE <glarivie@e2r1p3.42.fr>
2016-11-01 20:47:48 +0100

    norme

--- j04/ex01/ft_recursive_factorial.c
+++ j04/ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: glarivie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/09 11:51:51 by glarivie          #+#    #+#             */
-/*   Updated: 2015/08/11 16:50:32 by glarivie         ###   ########.fr       */
+/*   Updated: 2016/11/01 20:47:34 by glarivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
 		return (0);
-	if (nb == 0 || nb ==1)
+	if (nb == 0 || nb == 1)
 		return (1);
 	else
 		return (nb * ft_recursive_factorial(nb - 1));
